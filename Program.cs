26c9b0241bc42cd1800c17632b16cb94edccb25c
Dante Herrera <dherrera3@my.wctc.edu>
2024-04-25 12:45:53 -0500

    add data annotation and validation for blogs

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using NLog;
 using System.Linq;
+using System.ComponentModel.DataAnnotations;
 
 // See https://aka.ms/new-console-template for more information
 string path = Directory.GetCurrentDirectory() + "\\nlog.config";
@@ -53,8 +54,34 @@ try
 
                 var blog = new Blog { Name = name };
 
-                db.AddBlog(blog);
-                logger.Info($"Blog added - \"{name}\"");
+                ValidationContext context = new ValidationContext(blog, null, null);
+                List<ValidationResult> results = new List<ValidationResult>();
+
+                var isValid = Validator.TryValidateObject(blog, context, results, true);
+                if (isValid)
+                {
+                    // check for unique name
+                    if (db.Blogs.Any(b => b.Name == blog.Name))
+                    {
+                        // generate validation error
+                        isValid = false;
+                        results.Add(new ValidationResult("Blog name exists", new string[] { "Name" }));
+                    }
+                    else
+                    {
+                        logger.Info("Validation passed");
+                        // save blog to db
+                        db.AddBlog(blog);
+                        logger.Info("Blog added - {name}", blog.Name);
+                    }
+                }
+                if (!isValid)
+                {
+                    foreach (var result in results)
+                    {
+                        logger.Error($"{result.MemberNames.First()} : {result.ErrorMessage}");
+                    }
+                }
 
                 break;
 
